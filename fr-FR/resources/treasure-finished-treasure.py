098718d2ea3aa94d93ad40084c77c7121264ef87
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-08-14 14:40:50 +0100

    New translations treasure-finished-treasure.py (French)

--- fr-FR/resources/treasure-finished-treasure.py
+++ fr-FR/resources/treasure-finished-treasure.py
@@ -7,42 +7,42 @@ from random import randint
 sense = SenseHat()
 sense.clear()
 
-# Just return the actions we are interested in
-def wait_for_move():
+# On renvoie seulement les actions qui nous intéressent
+def attendre_mouvement():
   while True:
-    e = sense.stick.wait_for_event()
+    e = sense.stick.attendre_evenement()
     if e.action != ACTION_RELEASED:
       return e
 
-R = [255, 0, 0]  # red
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-W = [255, 255, 255] # white
+R = [255, 0, 0]  # rouge
+J = [255, 255, 0] # jaune
+V = [0, 255, 0] # vert
+B = [255, 255, 255] # blanc
 
 score = 0
 
-for turns in range(10):
+for tours in range(10):
   
-  coinx = randint(0,7)
-  coiny = randint(0,7)
-  print(coinx, coiny)
+  piecex = randint(0,7)
+  piecey = randint(0,7)
+  print(piecex, piecey)
     
-  sense.set_pixel(coinx, coiny, Y)
+  sense.set_pixel(piecex, piecey, J)
   sleep(2)
   sense.clear()
     
   x = randint(0,7)
   y = randint(0,7)
-  sense.set_pixel(x, y, W)
+  sense.set_pixel(x, y, B)
     
   while True:
   
-    e = wait_for_move()
+    e = attendre_mouvement()
     
     if e.direction == DIRECTION_MIDDLE:
       
-      if x == coinx and y == coiny:
-        sense.set_pixel(x, y, G)
+      if x == piecex and y == piecey:
+        sense.set_pixel(x, y, V)
         score += 1
       else:
         sense.set_pixel(x, y, R)
@@ -53,7 +53,7 @@ for turns in range(10):
   
     sense.clear()
       
-    if e.direction ==  DIRECTION_UP and y > 0:
+    if e.direction == DIRECTION_UP and y > 0:
       y = y - 1
       
     elif e.direction ==  DIRECTION_DOWN and y < 7:
@@ -62,11 +62,11 @@ for turns in range(10):
     elif e.direction ==  DIRECTION_LEFT and x > 0:
       x = x - 1
 
-    elif e.direction ==  DIRECTION_RIGHT and x < 7:
+    elif e.direction == DIRECTION_RIGHT and x <7:
       x = x + 1
 
-    sense.set_pixel(x, y, W)
+    sense.set_pixel(x, y, B)
       
-sense.show_message("Score: " + str(score))       
+sense.show_message("Score : " + str(score))       
   
   
