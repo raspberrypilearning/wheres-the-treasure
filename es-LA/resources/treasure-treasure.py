594c171a33e64d163f613b3404690c9dc69f10b9
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-01-20 13:14:24 +0000

    New translations treasure-treasure.py (Spanish, Latin America)

--- es-LA/resources/treasure-treasure.py
+++ es-LA/resources/treasure-treasure.py
@@ -7,15 +7,15 @@ from random import randint
 sense = SenseHat()
 sense.clear()
 
-# Just return the actions we are interested in
+# Solo devuelve las acciones que nos interesan
 def wait_for_move():
   while True:
     e = sense.stick.wait_for_event()
     if e.action != ACTION_RELEASED:
       return e
 
-R = [255, 0, 0]  # red
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-W = [255, 255, 255] # white
+R = [255, 0, 0]  # rojo
+A = [255, 255, 0] # amarillo
+V = [0, 255, 0] # verde
+B = [255, 255, 255] # blanco
 
