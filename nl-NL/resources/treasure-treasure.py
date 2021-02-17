9d9424c94f14862bc04965cac029ba651ba8a273
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-02-17 11:47:36 +0000

    New translations treasure-treasure.py (Dutch)

--- nl-NL/resources/treasure-treasure.py
+++ nl-NL/resources/treasure-treasure.py
@@ -7,15 +7,15 @@ from random import randint
 sense = SenseHat()
 sense.clear()
 
-# Just return the actions we are interested in
-def wait_for_move():
+# Geef gewoon de acties terug waarin we geïnteresseerd zijn
+def wacht_op_zet():
   while True:
     e = sense.stick.wait_for_event()
     if e.action != ACTION_RELEASED:
       return e
 
-R = [255, 0, 0]  # red
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-W = [255, 255, 255] # white
+R = [255, 0, 0]  # rood
+Y = [255, 255, 0] # geel
+G = [0, 255, 0] # groen
+W = [255, 255, 255] # wit
 
